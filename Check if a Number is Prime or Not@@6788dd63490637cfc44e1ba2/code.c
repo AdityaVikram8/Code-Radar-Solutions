11b0abaadd08baf45f2f47c477cbf7cd63628a3b
AdityaVikram8 <aditya0247.becse24@chitkara.edu.in>
2025-02-14 17:32:33 +0530

    Solution to Check if a Number is Prime or Not

--- Check if a Number is Prime or Not@@6788dd63490637cfc44e1ba2/code.c
+++ Check if a Number is Prime or Not@@6788dd63490637cfc44e1ba2/code.c
@@ -9,7 +9,7 @@ int main(){
     if(a<=1){
         isflag=false;
     }else{
-        for(int i=2;i<=sqrt(a);i++){
+        for(int i=2;i<=floor(sqrt(a));i++){
             if(a%i==0){
                 isflag=false;
                 break;
