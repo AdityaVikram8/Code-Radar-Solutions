2ff9a91ac9cbce6b74446795f6ecbc23ff0f2812
AdityaVikram8 <aditya0247.becse24@chitkara.edu.in>
2025-02-16 12:04:46 +0530

    Solution to Toggle nth Bit of a Number

---
+++ Toggle nth Bit of a Number@@67882a731b4392d8fc1577ed/code.c
@@ -0,0 +1,21 @@
+// Your code here...
+#include <stdio.h>
+int main(){
+    int num,pos,bin[32],num1,sum=0;
+    scanf("%d %d",&num,&pos);
+    num1=num;
+    for(int i=0;i<num1;i++){
+        int rem=num%2;
+        bin[i]=rem;
+        num=num/10;
+    }
+    if(bin[pos]==1){
+        bin[pos]=0;
+    }else{
+        bin[pos]=1;
+    }
+    for(int i=0;i<num1;i++){
+        sum+=bin[i]*(1<<i);
+    }
+    printf("%d",sum);
+}
