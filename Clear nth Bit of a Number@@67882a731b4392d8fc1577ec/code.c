40d30f54e8d957d997fa3ec622ab0524caff9111
AdityaVikram8 <aditya0247.becse24@chitkara.edu.in>
2025-02-16 11:53:32 +0530

    Solution to Clear nth Bit of a Number

--- Clear nth Bit of a Number@@67882a731b4392d8fc1577ec/code.c
+++ Clear nth Bit of a Number@@67882a731b4392d8fc1577ec/code.c
@@ -1,32 +1,23 @@
+// Your code here...
 #include <stdio.h>
 #include <math.h>
-
-int main() {
-    int num, pos, bin[32];
-    double sum = 0.0;  // sum is a double to handle floating-point numbers
-    scanf("%d %d", &num, &pos);
-
-    int i = 0;
-    // Convert num to binary and store in bin[]
-    while (num > 0) {
-        bin[i] = num % 2;
-        num = num / 2;
-        i++;
+int main(){
+    int num,pos,bin[32],num1
+    double sum=0;
+    scanf("%d %d",&num,&pos);
+    num1=num;
+    for(int i=0;i<num1;i++){
+        int rem=num%2;
+        bin[i]=rem;
+        num=num/2;
     }
-
-    // Flip the bit at position pos
-    if (bin[pos] == 1) {
-        bin[pos] = 0;
-    } else {
-        bin[pos] = 1;
+    if(bin[pos]==1){
+        bin[pos]=0;
+    }else{
+        bin[pos]=1;
     }
-
-    // Convert the modified binary back to decimal using pow() (2^i)
-    for (int j = 0; j < i; j++) {
-        sum += bin[j] * pow(2, j);  // Use pow() to calculate powers of 2
+    for(int i=0;i<num1;i++){
+        sum+=(bin[i]*(1<<i));
     }
-
-    // Output the result as an integer
-    printf("%d\n", (int)sum);  // Cast the sum to an integer for printing
-    return 0;
+    printf("%d",(int)sum);
 }
