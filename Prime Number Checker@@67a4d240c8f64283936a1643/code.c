39224b3af04fff2cf718444f7098b72f1113134d
AdityaVikram8 <aditya0247.becse24@chitkara.edu.in>
2025-03-01 21:30:31 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -1,12 +1,12 @@
-int isPrime(int num) {
-    if (num <= 1) return 0; // 1 is not prime
-    if (num == 2 || num == 3) return 1; // 2 and 3 are prime
-    if (num % 2 == 0 || num % 3 == 0) return 0; // Exclude multiples of 2 and 3
 
-    for (int i = 5; i * i <= num; i += 2) { // Check odd numbers only
+int isPrime(num) {
+    if (num <= 1) {
+        return 0;
+    }
+    for (int i = 2; i <num; i++) {
         if (num % i == 0) {
             return 0;
         }
     }
-    return 1; // Prime
+    return 1;
 }
